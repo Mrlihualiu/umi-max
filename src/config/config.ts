15c94b2975e42ffa277fd8e5388c1e6eebac861c
liulihua <hc_liulihua@163.com>
2024-11-18 18:22:47 +0800

    feat: init

---
+++ src/config/config.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from '@umijs/max';
+
+export default defineConfig({
+  antd: {},
+  access: {},
+  model: {},
+  initialState: {},
+  request: {},
+  layout: {
+    title: '前台性能监控平台',
+  },
+  routes: [
+    {
+      path: '/',
+      redirect: '/home',
+    },
+    {
+      name: '首页',
+      path: '/home',
+      component: './Home',
+    },
+    {
+      name: '首页',
+      path: '/login',
+      component: './Login',
+      layout: false,
+    },
+    {
+      name: '权限演示',
+      path: '/access',
+      component: './Access',
+    },
+    {
+      name: ' CRUD 示例',
+      path: '/table',
+      component: './Table',
+    },
+  ],
+  npmClient: 'pnpm',
+});
